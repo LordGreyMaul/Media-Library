53564618fdbb46682ad6d4d5723155953e5657ab
Graham Alan Morby-Raybould <dolphinsfan55@outlook.com>
2016-05-23 19:16:31 +0100

    single file form post
    moved all form, elements to single file

--- suggest.php
+++ suggest.php
@@ -1,4 +1,21 @@
-<?php 
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+$name = $_POST["name"];
+$email = $_POST["email"];
+$details = $_POST["details"];
+
+echo "<pre>";
+$email_body = "";
+$email_body .= "Name " . $name . "\n";
+$email_body .= "Email " . $email . "\n";
+$email_body .= "Details " . $details . "\n";
+echo $email_body;
+echo "</pre>";
+
+// To Do: Send Email
+header("location:suggest.php?status=thanks");
+}
+
 $pageTitle = "Suggest a Media Item";
 $section = "suggest";
 
@@ -7,15 +24,18 @@ include("inc/header.php"); ?>
 <div class="section page">
     <div class="wrapper">
         <h1>Suggest a Media Item</h1>
+        <?php if (isset($_GET["status"]) && $_GET["status"] == "thanks"){
+            echo "<p>Thanks for the email! I&rsquo;ll check out your suggestion shortly!</p>";
+        } else{ ?>
         <p>If you think there is something I&rsquo;m missing, Let me know! Complete the form to send me an email</p>
-        <form method="post" action="process.php">
+        <form method="post" action="suggest.php">
             <table>
                 <tr>
                     <th><label for="name">Name</label></th>
                     <td><input type="text" name="name" id="name" /></td>
                 </tr>
                 <tr>
-                    <th><label for="email">EMail</label></th>
+                    <th><label for="email">Email</label></th>
                     <td><input type="email" name="email" id="email" /></td>
                 </tr>
                 <tr>
@@ -25,7 +45,7 @@ include("inc/header.php"); ?>
             </table>
             <input type="submit" value="Send"  />
         </form>
+       <?php } ?>
     </div>
-
 </div>
 <?php include("inc/footer.php"); ?>
