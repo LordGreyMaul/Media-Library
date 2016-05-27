72a7a6825a06be726bb611fabbad5b55555da98c
Graham Alan Morby-Raybould <dolphinsfan55@outlook.com>
2016-05-27 12:55:54 +0100

    Added  new verfication

--- suggest.php
+++ suggest.php
@@ -2,10 +2,15 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
 $name = trim(filter_input(INPUT_POST, "name", FILTER_SANITIZE_STRING ));
 $email = trim(filter_input(INPUT_POST,"email", FILTER_SANITIZE_EMAIL));
+$category = trim(filter_input(INPUT_POST, "category", FILTER_SANITIZE_STRING ));
+$title = trim(filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING ));
+$format = trim(filter_input(INPUT_POST, "format", FILTER_SANITIZE_STRING ));
+$genre = trim(filter_input(INPUT_POST, "genre", FILTER_SANITIZE_STRING ));
+$year = trim(filter_input(INPUT_POST, "year", FILTER_SANITIZE_STRING ));
 $details = trim(filter_input( INPUT_POST,"details", FILTER_SANITIZE_SPECIAL_CHARS));
 
-    if ($name == "" || $email == "" || $details == ""){
-        echo "Please fill out the field: Name, Email and Details";
+    if ($name == "" || $email == "" || $category == "" || $title == ""){
+        echo "Please fill out the field: Name, Email, Category and Title";
         exit;
     }
     if ($_POST["address"] != "") {
@@ -16,26 +21,28 @@ $details = trim(filter_input( INPUT_POST,"details", FILTER_SANITIZE_SPECIAL_CHAR
     require ("inc/phpmailer/class.phpmailer.php");
 
     $mail = new PHPMailer;
-    if ($mail->ValidateAddress($email)) {
-        echo "Invalid Email Address";
-        exit;
-    }
+
 
 echo "<pre>";
 $email_body = "";
 $email_body .= "Name " . $name . "\n";
 $email_body .= "Email " . $email . "\n";
+$email_body .= "Suggested item\n";
+$email_body .= "Category " . $category . "\n";
+$email_body .= "Title " . $title . "\n";
+$email_body .= "Format " . $format . "\n";
+$email_body .= "Genre " . $genre . "\n";
+$email_body .= "Year " . $year . "\n";
 $email_body .= "Details " . $details . "\n";
 
     $mail->setFrom('$email', '$name');
-    $mail->addAddress('treehouse@localhost', 'Graham');     // Add a recipient
+    $mail->addAddress('graham@morby-raybould.com', 'Graham');     // Add a recipient
     $mail->isHTML(false);                                  // Set email format to HTML
 
     $mail->Subject = 'Personal Media Lib Suggest' . $name;
     $mail->Body    = $email_body;
 
-
-    if(!$mail->send()) {
+        if(!$mail->send()) {
         echo 'Message could not be sent.';
         echo 'Mailer Error: ' . $mail->ErrorInfo;
         exit;
@@ -59,15 +66,131 @@ include("inc/header.php"); ?>
         <form method="post" action="suggest.php">
             <table>
                 <tr>
-                    <th><label for="name">Name</label></th>
+                    <th><label for="name">Name (Required)</label></th>
                     <td><input type="text" name="name" id="name" /></td>
                 </tr>
                 <tr>
-                    <th><label for="email">Email</label></th>
+                    <th><label for="email">Email (Required)</label></th>
                     <td><input type="email" name="email" id="email" /></td>
                 </tr>
                 <tr>
-                    <th><label for="details">Suggest Item Details</label></th>
+                    <th><label for="category">Category (Required)</label></th>
+                    <td>
+                        <select name="category" id="category">
+                            <option vaule="">Select One</option>
+                            <option vaule="Book">Book</option>
+                            <option vaule="Movies">Movie</option>
+                            <option vaule="Music">Music</option>
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <th><label for="title">Title (Required)</label></th>
+                    <td><input type="title" name="title" id="title" /></td>
+                </tr>
+                <tr>
+                    <th><label for="format">Format</label></th>
+                    <td><select id="format" name="format">
+                            <option value="">Select One</option>
+                            <optgroup label="Books">
+                                <option value="Audio">Audio</option>
+                                <option value="Ebook">Ebook</option>
+                                <option value="Hardback">Hardback</option>
+                                <option value="Paperback">Paperback</option>
+                            </optgroup>
+                            <optgroup label="Movies">
+                                <option value="Blu-ray">Blu-ray</option>
+                                <option value="DVD">DVD</option>
+                                <option value="Streaming">Streaming</option>
+                                <option value="VHS">VHS</option>
+                            </optgroup>
+                            <optgroup label="Music">
+                                <option value="Cassette">Cassette</option>
+                                <option value="CD">CD</option>
+                                <option value="MP3">MP3</option>
+                                <option value="Vinyl">Vinyl</option>
+                            </optgroup>
+                        </select></td>
+                </tr>
+                <tr>
+                <tr>
+                    <th><label for="format">Genre</label></th>
+                    <td>
+                        <select name="genre" id="genre">
+                            <option value="">Select One</option>
+                            <optgroup label="Books">
+                                <option value="Action">Action</option>
+                                <option value="Adventure">Adventure</option>
+                                <option value="Comedy">Comedy</option>
+                                <option value="Fantasy">Fantasy</option>
+                                <option value="Historical">Historical</option>
+                                <option value="Historical Fiction">Historical Fiction</option>
+                                <option value="Horror">Horror</option>
+                                <option value="Magical Realism">Magical Realism</option>
+                                <option value="Mystery">Mystery</option>
+                                <option value="Paranoid">Paranoid</option>
+                                <option value="Philosophical">Philosophical</option>
+                                <option value="Political">Political</option>
+                                <option value="Romance">Romance</option>
+                                <option value="Saga">Saga</option>
+                                <option value="Satire">Satire</option>
+                                <option value="Sci-Fi">Sci-Fi</option>
+                                <option value="Tech">Tech</option>
+                                <option value="Thriller">Thriller</option>
+                                <option value="Urban">Urban</option>
+                            </optgroup>
+                            <optgroup label="Movies">
+                                <option value="Action">Action</option>
+                                <option value="Adventure">Adventure</option>
+                                <option value="Animation">Animation</option>
+                                <option value="Biography">Biography</option>
+                                <option value="Comedy">Comedy</option>
+                                <option value="Crime">Crime</option>
+                                <option value="Documentary">Documentary</option>
+                                <option value="Drama">Drama</option>
+                                <option value="Family">Family</option>
+                                <option value="Fantasy">Fantasy</option>
+                                <option value="Film-Noir">Film-Noir</option>
+                                <option value="History">History</option>
+                                <option value="Horror">Horror</option>
+                                <option value="Musical">Musical</option>
+                                <option value="Mystery">Mystery</option>
+                                <option value="Romance">Romance</option>
+                                <option value="Sci-Fi">Sci-Fi</option>
+                                <option value="Sport">Sport</option>
+                                <option value="Thriller">Thriller</option>
+                                <option value="War">War</option>
+                                <option value="Western">Western</option>
+                            </optgroup>
+                            <optgroup label="Music">
+                                <option value="Alternative">Alternative</option>
+                                <option value="Blues">Blues</option>
+                                <option value="Classical">Classical</option>
+                                <option value="Country">Country</option>
+                                <option value="Dance">Dance</option>
+                                <option value="Easy Listening">Easy Listening</option>
+                                <option value="Electronic">Electronic</option>
+                                <option value="Folk">Folk</option>
+                                <option value="Hip Hop/Rap">Hip Hop/Rap</option>
+                                <option value="Inspirational/Gospel">Insirational/Gospel</option>
+                                <option value="Jazz">Jazz</option>
+                                <option value="Latin">Latin</option>
+                                <option value="New Age">New Age</option>
+                                <option value="Opera">Opera</option>
+                                <option value="Pop">Pop</option>
+                                <option value="R&B/Soul">R&amp;B/Soul</option>
+                                <option value="Reggae">Reggae</option>
+                                <option value="Rock">Rock</option>
+                            </optgroup>
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <th><label for="year">Year</label></th>
+                    <td><input type="year" name="year" id="year" /></td>
+                </tr>
+                <tr>
+                    <th><label for="details">Additional Details</label></th>
                     <td><textarea name="details" id="details"></textarea>
                 </tr>
                 <tr style="display: none;">
